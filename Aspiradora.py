78980595976d7b9a248b454716d14c9018d3d86a
Efren73 <A01276266@itesm.mx>
2021-11-09 02:08:20 -0600

    Falta comentar algunas cosas

--- Aspiradora.py
+++ Aspiradora.py
@@ -5,6 +5,7 @@
 import random
 import time
 
+#Clase para identificar las coordenadas x y y
 class Punto:
     def __init__(self, x, y): 
         self.x = 0 
@@ -68,28 +69,35 @@ while(porcentaje > 0 and tiempo <= tmax):
     inicio = time.time()
     for i in range (cant_agentes):
         if(habitaciones[agentes[i].x][agentes[i].y] == 1):
-            print("Agente "+str(i+1)+" limpiando la celda ("+str(agentes[i].x)+ ", " +str(agentes[i].y)+")")
+            print("Agente "+str(i+1)+" limpiando la celda ("+str(agentes[i].x + 1)+ ", " +str(agentes[i].y + 1)+")")
             habitaciones[agentes[i].x][agentes[i].y] = 0
             porcentaje -= 1
             visitadas[agentes[i].x][agentes[i].y] = 1
             ciclo = True
             posx = -1
             posy = -1
-            count = 0
+            count = 1
             while(ciclo == True):
                 
                 if((agentes[i].x+posx) >= 0 and
                    (agentes[i].x+posx) <= (filas-1) and
                    (agentes[i].y+posy) >= 0 and
                    (agentes[i].y+posy) <= (columnas-1) and
-                   visitadas[agentes[i].x + posx][agentes[i].y + posy] == 0):
-                    agentes[i].x += posx
-                    agentes[i].y += posy
-                    print("Moviendo agente " +str(i+1)+ " a la casilla ("+str([agentes[i].x])+", "+str([agentes[i].y])+")")
-                    movimientos += 1
-                    ciclo = False
+                   visitadas[agentes[i].x + posx][agentes[i].y + posy] == 0 and count<=9):
+                    cont_temporal = 0
+                    for j in range (cant_agentes):
+                        if(i == j):
+                            continue
+                        elif(agentes[i].x + posx == agentes[j].x and agentes[i].y + posy == agentes[j].y):
+                            cont_temporal += 1
+                    if(cont_temporal == 0):
+                        agentes[i].x += posx
+                        agentes[i].y += posy
+                        print("Moviendo agente " +str(i+1)+ " a la casilla ("+str(agentes[i].x +1)+", "+str(agentes[i].y + 1)+")")
+                        movimientos += 1
+                        ciclo = False
                 
-                elif(count == 7):
+                elif(count > 9):
                     rand_val_1 = random.randint(-1, 1)
                     rand_val_2 = random.randint(-1, 1)
                     if((agentes[i].x+rand_val_1) < 0 or
@@ -98,39 +106,56 @@ while(porcentaje > 0 and tiempo <= tmax):
                        (agentes[i].y+rand_val_2) > (columnas-1) or
                        (rand_val_1 == 0 and rand_val_2 == 0)):
                         ciclo = False
-                    else: 
-                      agentes[i].x += rand_val_1
-                      agentes[i].y += rand_val_2
-                      print("Moviendo agente " +str(i+1)+ " a la casilla ("+str([agentes[i].x])+", "+str([agentes[i].y])+")")
-                      movimientos += 1
-                      ciclo = False
-                    
-                elif(count%2 == 0):
-                    posx+=1
-                    count+=1
-                elif(count%2 == 1):
-                    posy+=1
-                    count+=1
+                    else:
+                        cont_temporal = 0
+                        for j in range (cant_agentes):
+                            if(i == j):
+                                continue
+                            elif(agentes[i].x + posx == agentes[j].x and agentes[i].y + posy == agentes[j].y):
+                                cont_temporal += 1
+                        if(cont_temporal == 0):
+                            agentes[i].x += rand_val_1
+                            agentes[i].y += rand_val_2
+                            print("Moviendo agente " +str(i+1)+ " a la casilla ("+str(agentes[i].x +1)+", "+str(agentes[i].y + 1)+")")
+                            movimientos += 1
+                            ciclo = False
+                        else:
+                            ciclo = False
+
+                if(count%3 == 0):
+                    posy= -1
+                    posx += 1
+                else:
+                    posy += 1
+                if(ciclo == True):
+                    count += 1                        
               
         elif(habitaciones[agentes[i].x][agentes[i].y] == 0):
             visitadas[agentes[i].x][agentes[i].y] = 1
             ciclo = True
             posx = -1
             posy = -1
-            count = 0
+            count = 1
             while(ciclo == True):
                 if((agentes[i].x+posx) >= 0 and
                    (agentes[i].x+posx) <= (filas-1) and
                    (agentes[i].y+posy) >= 0 and
                    (agentes[i].y+posy) <= (columnas-1) and
-                   visitadas[agentes[i].x +posx][agentes[i].y+posy] == 0):
-                    agentes[i].x += posx
-                    agentes[i].y += posy
-                    print("Moviendo agente " +str(i+1)+ " a la casilla ("+str([agentes[i].x])+", "+str([agentes[i].y])+")")
-                    movimientos += 1
-                    ciclo = False
+                   visitadas[agentes[i].x + posx][agentes[i].y + posy] == 0 and count<=9):
+                    cont_temporal = 0
+                    for j in range (cant_agentes):
+                        if(i == j):
+                            continue
+                        elif(agentes[i].x + posx == agentes[j].x and agentes[i].y + posy == agentes[j].y):
+                            cont_temporal += 1
+                    if(cont_temporal == 0):
+                        agentes[i].x += posx
+                        agentes[i].y += posy
+                        print("Moviendo agente " +str(i+1)+ " a la casilla ("+str(agentes[i].x +1)+", "+str(agentes[i].y + 1)+")")
+                        movimientos += 1
+                        ciclo = False
                 
-                elif(count == 7):
+                elif(count > 9):
                     rand_val_1 = random.randint(-1, 1)
                     rand_val_2 = random.randint(-1, 1)
                     if((agentes[i].x+rand_val_1) < 0 or
@@ -139,19 +164,30 @@ while(porcentaje > 0 and tiempo <= tmax):
                        (agentes[i].y+rand_val_2) > (columnas-1) or
                        (rand_val_1 == 0 and rand_val_2 == 0)):
                         ciclo = False
-                    else: 
-                      agentes[i].x += rand_val_1
-                      agentes[i].y += rand_val_2
-                      print("Moviendo agente " +str(i+1)+ " a la casilla ("+str([agentes[i].x])+", "+str([agentes[i].y])+")")
-                      movimientos += 1
-                      ciclo = False
+                    else:
+                        cont_temporal = 0
+                        for j in range (cant_agentes):
+                            if(i == j):
+                                continue
+                            elif(agentes[i].x + posx == agentes[j].x and agentes[i].y + posy == agentes[j].y):
+                                cont_temporal += 1
+                        if(cont_temporal == 0):
+                            agentes[i].x += rand_val_1
+                            agentes[i].y += rand_val_2
+                            print("Moviendo agente " +str(i+1)+ " a la casilla ("+str(agentes[i].x +1)+", "+str(agentes[i].y + 1)+")")
+                            movimientos += 1
+                            ciclo = False
+                        else:
+                            ciclo = False
+
+                if(count%3 == 0):
+                    posy= -1
+                    posx += 1
+                else:
+                    posy += 1
+                if(ciclo == True):
+                    count += 1
                     
-                elif(count%2 == 0):
-                    posx+=1
-                    count+=1
-                elif(count%2 == 1):
-                    posy+=1
-                    count+=1
                     
     fin = time.time()
     tiempo += (fin-inicio)
@@ -171,4 +207,4 @@ porcentaje_final = float((limpias / (filas*columnas))*100)
 
 print("Tiempo que se registro en la limpieza de las habitaciones: ", tiempo)
 print("El porcentaje de habitaciones limpias es de: ", porcentaje_final)
-print("Movimientos realizados por el agente: ", movimientos)
+print("Movimientos realizados por los agentes: ", movimientos)
